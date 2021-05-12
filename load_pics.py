e9cf25d310317d4ceabb549dd2d8fdd370453cc6
rn1stc <31245403+rn1stc@users.noreply.github.com>
2021-05-12 21:53:37 +0300

    Add files via upload

---
+++ load_pics.py
@@ -0,0 +1,18 @@
+from datetime import datetime
+import instaloader
+
+L = instaloader.Instaloader()
+
+posts = instaloader.Hashtag.from_name(L.context, "urbanphotography").get_posts()
+
+k = 0  # initiate k
+
+count = 0
+for post in posts:
+    count += 1
+    #L.download_post(post, "#urbanphotography")
+    print(post.url)
+
+    if count == 15:
+        break
+
