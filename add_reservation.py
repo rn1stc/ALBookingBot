ae3866b9d58f1cd567380c5c440f37146e2a6bd5
rn1stc <31245403+rn1stc@users.noreply.github.com>
2021-05-12 21:18:36 +0300

    Add files via upload

--- add_reservation.py
+++ add_reservation.py
@@ -1,3 +1,4 @@
+<<<<<<< HEAD
 #!/usr/bin/python
 
 import psycopg2
@@ -39,4 +40,47 @@ def insert_reservation(house, id, check_in_date, check_in_time, check_out_date, 
     return reservation_id
 
 insert_reservation('emerald', 121, '03/03/2014', '02:03:04', '03/03/2014',
+=======
+#!/usr/bin/python
+
+import psycopg2
+import datetime
+from config import config
+
+
+
+def insert_reservation(house, id, check_in_date, check_in_time, check_out_date, guest_name,
+                       guest_cell, guest_telegram, num_guest, comment, confirm):
+    """ insert a new reservation into the reservation table """
+    sql = """INSERT INTO %s VALUES(%s, '%s', '%s', '%s', '%s', '%s', '%s', %s, '%s', %s) RETURNING reservation_id;"""
+    conn = None
+    reservation_id = None
+    try:
+        # read database configuration
+        params = config()
+        # connect to the PostgreSQL database
+        conn = psycopg2.connect(**params)
+        # create a new cursor
+        cur = conn.cursor()
+        # execute the INSERT statement
+        print(sql % (house, id, check_in_date, check_in_time, check_out_date, guest_name,
+                       guest_cell, guest_telegram, num_guest, comment, confirm))
+        cur.execute(sql, (house, id, check_in_date, check_in_time, check_out_date, guest_name,
+                       guest_cell, guest_telegram, num_guest, comment, confirm))
+        # get the generated id back
+        vendor_id = cur.fetchone()[0]
+        # commit the changes to the database
+        conn.commit()
+        # close communication with the database
+        cur.close()
+    except (Exception, psycopg2.DatabaseError) as error:
+        print(error)
+    finally:
+        if conn is not None:
+            conn.close()
+
+    return reservation_id
+
+insert_reservation('emerald', 121, '03/03/2014', '02:03:04', '03/03/2014',
+>>>>>>> origin/master
                    'Jack of Blades', '+79150840000', 'noo', 2, 'quite room please', 'false')
